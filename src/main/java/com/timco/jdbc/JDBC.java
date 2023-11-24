1139f324d5cb314f45bd2e473f9dc42173d3ecd8
brigitte <mbpadilla4@misena.edu.co>
2023-11-24 14:11:11 -0500

    Iniciando proyecto

---
+++ src/main/java/com/timco/jdbc/JDBC.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.timco.jdbc;
+
+/**
+ *
+ * @author brigi
+ */
+public class JDBC {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}

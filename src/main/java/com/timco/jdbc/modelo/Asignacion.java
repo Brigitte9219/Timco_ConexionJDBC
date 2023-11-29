d2a30ba893148d65d80d55a3098255d39cd2b872
brigitte <brigitte@DESKTOP-UUB7I6H>
2023-11-29 09:30:47 -0500

    Creación de la clase Asignacion

---
+++ src/main/java/com/timco/jdbc/modelo/Asignacion.java
@@ -0,0 +1,20 @@
+
+package com.timco.jdbc.modelo;
+
+
+public class Asignacion {
+    
+    private int idasignacion;
+    private Date Fecha_Asignacion;
+    private int Nombre_Trabajador;
+    private int trabajador_Numero_ID;
+    private String producto_Nombre_Producto;
+    private int Cantidad;
+    private String Talla;
+    private String Proyecto;
+    private String Area;
+    private String Observaciones;
+    private int usuarios_idusuario;
+        
+       
+}

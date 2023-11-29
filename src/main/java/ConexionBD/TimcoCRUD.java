6f2689f4ec4609ad765f8eeabda9f6298ca2ba2e
brigitte <brigitte@DESKTOP-UUB7I6H>
2023-11-29 17:27:39 -0500

    Actualizando el método Agregar Trabajador

--- src/main/java/ConexionBD/TimcoCRUD.java
+++ src/main/java/ConexionBD/TimcoCRUD.java
@@ -3,6 +3,7 @@ package ConexionBD;
 
 import CRUD.TrabajadorCRUD;
 import com.timco.jdbc.modelo.Trabajador;
+import java.sql.Date;
 
 
 public class TimcoCRUD {
@@ -12,8 +13,42 @@ public class TimcoCRUD {
         // TODO code application logic here
         
         Trabajador trabajador =new Trabajador();
-        trabajador.setNombre_Completo("Brigitte");
+        
+        trabajador.setNombre_Completo("Brigitte Padilla");
+        trabajador.setCorreo_Electronico("brigittep29_@hotmail.com");
+        trabajador.setTipo_ID("CC");
         trabajador.setNumero_ID(1118551584);
+        trabajador.setFecha_Expedicion(Date.valueOf("2010-08-12"));
+        trabajador.setCiudad_Expedicion("Yopal");
+        trabajador.setFecha_Nacimiento(Date.valueOf("1992-08-05"));
+        trabajador.setLugar_Nacimiento("Bogota");
+        trabajador.setEdad(31);
+        trabajador.setEstatura(1.60);
+        trabajador.setGenero("Femenino");
+        trabajador.setEstado_Civil("Soltera");
+        trabajador.setRH("+");
+        trabajador.setGrupo_Sanguineo("O");
+        trabajador.setLocalidad("Kennedy");
+        trabajador.setCiudad_Residencia("Bogota");
+        trabajador.setBarrio("Andalucia");
+        trabajador.setDireccion("Av Cali con Calle 13");
+        trabajador.setTelefono_Movil("3118677520");
+        trabajador.setTelefono_Fijo("0");
+        trabajador.setEPS("Sanitas");
+        trabajador.setAFP("Colpensiones");
+        trabajador.setEscolaridad("Posgrado");
+        trabajador.setProfesion("Ingeniera");
+        trabajador.setTalla_Camisa("L");
+        trabajador.setTalla_Pantalon("16");
+        trabajador.setTalla_Chaqueta("L");
+        trabajador.setTalla_Calzado("38");
+        trabajador.setEmergencia_NombreCompleto("Martha Salgado");
+        trabajador.setEmergencia_Parentesco("Madre");
+        trabajador.setEmergencia_TelefonoMovil("3114890254");
+        trabajador.setEmergencia_TelefonoFijo("0");
+        
+        
+        
         TrabajadorCRUD trabajadorcrud= new TrabajadorCRUD();
         
         //Agregar trabajador
